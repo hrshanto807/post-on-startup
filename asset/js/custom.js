4823af0a3ed0c86f1afbb92f3f7a7c5671a252ba
hrsanto <shantosss4907@gmail.com>
2023-12-20 18:08:06 +0600

    home-page html done

---
+++ asset/js/custom.js
@@ -0,0 +1,58 @@
+(function ($) {
+    $('#icon').click(function () {
+        $('.pos-primary-menu ul,.pos-header-button').slideToggle();
+
+    });
+    $(window).resize(function () {
+        let windowSize = $(window).width();
+        if (windowSize > 600) {
+            $('.pos-primary-menu ul,.pos-header-button').removeAttr('style');
+        }
+    });
+    $('.pos-all-single-atuhors-area').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 10,
+
+            },
+            600: {
+                items: 1,
+                margin: 20,
+
+            },
+            1000: {
+                items: 4,
+            }
+        },
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+    });
+    $('.pos-testimonial-content-area').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        items: 1,
+        navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>'],
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+    });
+    // ------------------------------------
+    $('#icon-2').click(function () {
+        $('.pos-secondary-menu ul').slideToggle();
+
+    });
+    $(window).resize(function () {
+        let windowSize = $(window).width();
+        if (windowSize > 600) {
+            $('.pos-secondary-menu ul').removeAttr('style');
+        }
+    });    
+    
+
+})(jQuery);
